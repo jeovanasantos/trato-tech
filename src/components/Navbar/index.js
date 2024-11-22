d239edc0834e46b9062d1b0beee8cb6309272372
Usuario <jeovanasantosdacosta25@gmail.com>
2024-11-22 15:44:48 -0400

    react-icons

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,28 @@
 import styles from './Navbar.module.scss';
 import { ReactComponent as Logo} from '../../assets/logo.svg'
+import classNames from 'classnames';
+
 export default function Navbar(){
 return(
     <nav className={styles.nav}>
         <Logo className={styles.logo}/>
+        <div className={styles.links}>
+            <div>
+                <a href='/' className={styles.link}>
+                Página inicial</a>
+            </div>
+        </div>
+        <div className={styles.busca}>
+
+        </div>
+        <div className={styles.icones}>
+            <a href='/carrinho'>
+            {window.location.pathname === '/carrinho' 
+            ?
+            :
+            }
+            </a>
+        </div>
     </nav>
 )
 }
