551bc07df408910fa48a803ef889772669ac5a1d
jeovana santos <jeovanasantosdacosta25@gmail.com>
2025-01-14 09:48:17 -0400

    página de categorias

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
 import PaginaPadrao from 'components/PaginaPadrao';
 import Home from './styles/pages/Home/index'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Categoria from './styles/pages/Categoria/index';
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<PaginaPadrao />}>
-          <Route index element={<Home/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/' element={<PaginaPadrao />}>
+        <Route index element={<Home />} />
+        <Route path='/categoria/:nomeCategoria' element={<Categoria />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
   )
 }
